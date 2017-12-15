0b16cbb0c5dee7389222fb33a553a6a92b173e6a
Nadyav984 <nadiav984@gmail.com>
2017-12-15 22:09:29 +0200

    added BD and admin site

--- server.js
+++ server.js
@@ -14,24 +14,26 @@ app.use('/assets', express.static('assets'))
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
-app.route(['/products', '/products/:id'])
+app.get('/admin', function (req, res) {
+  res.sendFile(path.join(__dirname + '/admin.html'));
+});
+app.route(['/products'])
   .get((req, res) => {
-    if (req.params.id) {
-      // let product = function();
-      return res.json({
-        err: 'not implemented'
-      })
+    let latest = req.query.latest;
+    let sql_query_we_want_to_execute = `SELECT p.name, p.price, i.path as image, s.discount FROM shop.products p
+                LEFT JOIN shop.img i ON p.img_id = i.id
+                LEFT JOIN shop.sale s ON p.id = s.product_id`
+    if(latest) {
+      sql_query_we_want_to_execute += ' WHERE created_at>= (CURDATE() - INTERVAL 2 DAY)'
+     
     }
-
-
-    connection.query(`SELECT p.name, p.price, i.path as image, s.discount FROM shop.products p
-LEFT JOIN shop.img i ON p.img_id = i.id
-LEFT JOIN shop.sale s ON p.id = s.product_id`, function (error, results, fields) {
+    connection.query(sql_query_we_want_to_execute, function (error, results, fields) {
       if (error) {
         return res.status(500).json(error)
       }
       return res.json(results)
     });
+    
   })
 app.listen(8080,(params) => {
   console.log('Magic happens on 8080')
